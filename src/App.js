9aa2032ee0e30990c9dfbcaa78489643c8f1b54a
zahra pourfakhar <zp.fakhar1996@gmail.com>
2021-08-12 18:00:24 -0400

    Enhance heather components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Headers/Header";
-import Contact from "./Pages/Contact";
-import Home from './Pages/Home';
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
 function App() {
