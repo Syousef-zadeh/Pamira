8c1072494286e1de01ff4797edc57ce6a7704f82
Sheida <syousef-zadeh@myseneca.ca>
2021-08-17 09:57:15 -0400

    solve completed(navbar component)

---
+++ src/components/Headers/components/Top-Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import AppointmentButton from "./AppointmentButton";
+import { BiPhoneCall } from "react-icons/bi";
+import { AiFillInstagram } from "react-icons/ai";
+import styles from "./Top-Header.module.css";
+
+const TopHeader = (props) => {
+  return (
+    <div className={styles.topHeader}>
+      <div className={styles.info}>
+        <BiPhoneCall className={styles.icons} />
+        <span>021-33307551</span>
+        
+        <AiFillInstagram className={styles.icons} />
+        <span className={styles.address}>Pamira.clinic</span>
+      </div>
+
+      <div className={styles.btn}>
+        <AppointmentButton />
+      </div>
+    </div>
+  );
+};
+export default TopHeader;
