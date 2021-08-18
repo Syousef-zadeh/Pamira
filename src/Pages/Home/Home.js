3e903eadb45446ae0e557fde5636fafb7114a047
Sheida <syousef-zadeh@myseneca.ca>
2021-08-18 18:28:18 -0400

    solve completed(navbar component)

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,10 +6,13 @@ import st from "./Home.module.css";
 import img1 from "../../assets/skincare3.jpg";
 import img2 from "../../assets/massage.jpg";
 import img3 from "../../assets/skincare.jpg";
+import Services from "./components/Services";
 
 const Home = () => {
   return (
     <div>
+      <div>
+
       <Carousel variant="dark" className={st.homeCarousel}>
         <Carousel.Item>
           <img className="d-block w-100" src={img2} alt="First slide" />
@@ -35,6 +38,8 @@ const Home = () => {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+      </div>
+      <Services />
     </div>
   );
 };
