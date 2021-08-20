27fc81192ce02c9cdc769f46de48d0a3d1b4c12f
Sheida <syousef-zadeh@myseneca.ca>
2021-08-19 20:12:10 -0400

    add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,11 @@
 import React from "react";
+import SocialMedia from "./component/SocialMedia";
 
 const Footer = () => {
-    
+    return(
+        <div>
+            <SocialMedia />
+        </div>
+    )
 };
 export default Footer;
---
+++ src/components/Footer/component/SocialMedia.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { ImYoutube2 } from "react-icons/im";
+import { AiFillInstagram, AiOutlineWhatsApp } from "react-icons/ai";
+
+const SocialMedia = () => {
+  return (
+    <div>
+      <div>
+        <ImYoutube2 />
+        <AiFillInstagram />
+        <AiOutlineWhatsApp />
+      </div>
+    </div>
+  );
+};
+export default SocialMedia;
