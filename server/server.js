13d48f7c57ecf4bcc07c984e312a5d9bda462972
Sheida <syousef-zadeh@myseneca.ca>
2021-08-29 12:01:10 -0400

    connnected to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// const uuid =require('uuid/v4');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const fileUpload = require('express-fileupload');
+const serviceModel = require('./model/service');
 require('dotenv').config({path:"../config/keys.env"});
 
 
 var app = express()
+app.use(cors());
 
+app.use(express.json());
 app.get('/', (req, res) => res.send('hello world'));
-
+app.post('/add-services', (req, res)=>{
+    console.log("Hi");
+    const newService =
+    {
+        serviceName: "Botox",
+        serviceDescription: "very very good!",
+        serviceImage: ""
+    }
+    const service = new serviceModel(newService);
+    console.log(service);
+    service.save();
+})
 
 app.use(fileUpload());
 app.use(session({
