09ab22e0863154f5f7c06539ddd08a7013d4bbb0
Sheida <syousef-zadeh@myseneca.ca>
2021-08-29 17:13:59 -0400

    change login css

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -3,15 +3,24 @@ import styles from "./login.module.css";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/button";
 
-
 const Login = () => {
   return (
     <div className={styles["login-form"]}>
-        <h4>Login Page</h4>
-      <div className={styles['login-border']}>
+      <h4>Login Page</h4>
+      <div className={styles["login-border"]}>
         <Input placeholder="Username" type="text" />
         <Input placeholder="password" type="password" />
-        <Button style={{backgroundColor: "#283b42", color: "white"}}>Login</Button>
+        <div className={styles.btn}>
+          <Button
+            style={{
+              backgroundColor: "#283b42",
+              color: "white",
+              padding: "4px 18px",
+            }}
+          >
+            Login
+          </Button>
+        </div>
       </div>
     </div>
   );
