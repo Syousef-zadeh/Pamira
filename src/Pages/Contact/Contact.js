3597b9606f7bcff213e9d8626e8cfba7cdc34c7c
Sheida <syousef-zadeh@myseneca.ca>
2021-09-01 20:54:10 -0400

    contact page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,8 +3,10 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <h3>Address: Tehran - 17 Shahrivar</h3>
-      <h3>Phone: 021-33307551</h3>
+      <div>
+        <h3>Address: Tehran - 17 Shahrivar</h3>
+        <h3>Phone: 021-33345247</h3>
+      </div>
     </div>
   );
 };
