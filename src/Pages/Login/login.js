b028d5bfa17f77955d539aa9504f8a96dfd00169
Sheida <syousef-zadeh@myseneca.ca>
2021-09-02 22:51:56 -0400

    feature content

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 import styles from "./login.module.css";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/button";
 import Loading from "../../components/Loading/LoadingPage";
-
-const Login = ({ setToken }) => {
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+ 
+const Login = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -29,67 +31,20 @@ const Login = ({ setToken }) => {
         },
         config
       );
-      console.log(data);
+      props.setToken(data);
       localStorage.setItem("userInfo", JSON.stringify(data));
       setLoading(false);
     } catch (error) {
       setError(error.response.data.message);
+      setLoading(false);
     }
   };
 
-  // const [username, setUsername] = useState();
-  // const [password, setPassword] = useState();
-  // const [login, setLogin] = useState([]);
-  // // const loginUser = async (credentials) => {
-  // //   // const [data, setData] = useState("");
-  // //    fetch('http://localhost:5000/dashboard', {
-  // //     method: 'POST',
-  // //     headers: {
-  // //       'Content-Type': 'application/json'
-  // //     },
-  // //     body: credentials
-  // //   })
-  // //     .then(data => {
-  // //       console.log(data);
-  // //       // data.json()
-  // //     }
-  // //       )
-
-  // // };
-
-  // const loginUser = (credentials) => {
-
-  //   try {
-  //     fetch("http://localhost:5000/dashboard", {
-  //           method: 'POST',
-  //           headers: {
-  //             'Content-Type': 'application/json'
-  //           },
-  //           body: JSON.stringify({username: credentials.username, password: credentials.password})
-  //         })
-  //       .then((res) => res.json())
-  //       .then((result) => {
-  //         console.log(result);
-  //         setLogin(result);
-  //       });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const token = await loginUser({
-  //     username,
-  //     password,
-  //   });
-  //   setToken(token);
-  // };
-
   return (
     <div>
+      {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
       {loading && <Loading />}
-      <form onSubmit={submitHandler}>
+      <form>
         <div className={styles["login-form"]}>
           <h4>Login Page</h4>
           <div className={styles["login-border"]}>
@@ -118,6 +73,7 @@ const Login = ({ setToken }) => {
                   padding: "4px 18px",
                 }}
                 type="submit"
+                onClick={submitHandler}
               >
                 Login
               </Button>
@@ -129,8 +85,6 @@ const Login = ({ setToken }) => {
   );
 };
 
-Login.propTypes = {
-  setToken: PropTypes.func.isRequired,
-};
+
 
 export default Login;
