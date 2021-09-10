744612d46daf743efdb65d51c8522c7109a285e1
Sheida <syousef-zadeh@myseneca.ca>
2021-09-10 17:54:48 -0400

    css dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,23 +4,26 @@ import Loading from "../../components/Loading/LoadingPage";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import { useHistory } from "react-router-dom";
 import Button from "../../components/UI/Button/button";
+
 import Input from "../../components/UI/Input/Input";
 
 const Dashboard = (props) => {
   const history = useHistory();
   const [fileName, setFileName] = useState("");
+  const [serviceName, setServiceName] = useState("");
   const [file, setFile] = useState("");
-  const [title, setTitle] = useState("");
   const [loading, setLoading] = useState(false);
 
   const [error, setError] = useState(false);
-  const onchange =async (e) => {
+
+  const onchangeFile =async (e) => {
     //setFileName(e.target.files[0]);
     const file = e.target.files[0];
+    console.log(e.target.files[0].name);
     const base64 = await converBase64(file);
     console.log(base64);
-    setFileName(base64);
-    // setFile(e.target.files[0].name)
+    setFile(base64);
+    setFileName(e.target.files[0].name)
   };
 
   const converBase64 =(file) =>{
@@ -35,35 +38,30 @@ const Dashboard = (props) => {
       }
     })
   }
-  console.log(fileName);
-  console.log(title);
+  console.log(typeof fileName);
+  console.log(serviceName);
+
 
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      const fb = new FormData();
-      fb.append("serviceName", title);
-      fb.append("serviceImage", fileName);
-
-      // const config = {
-      //   headers: {
-      //     "Content-Type": "multipart/form-data",
-      //   },
-      // };
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
       setLoading(true);
       const { data } = await axios.post(
         "http://localhost:5000/api/services/add",
-        fb
-        // "http://localhost:5000/api/services/add",
-        // {
-        //   file,
-        //   fileName,
-        //   title,
-        // },
-        // config
+        {
+          file,
+          fileName,
+          serviceName,
+        },
+        config
       );
-      console.log(data);
-      //localStorage.setItem("serviceInfo", JSON.stringify(data));
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
       setLoading(false);
     } catch (error) {
       setError(error.response.data.message);
@@ -78,36 +76,35 @@ const Dashboard = (props) => {
         <h5>Add a Service</h5>
         {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
         {loading && <Loading />}
-        <form encType="multipart/form-data">
-          <Input
-            placeholder="Service Title"
-            type="text"
-            filename="serviceName"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <Input
-            placeholder="Photo"
-            type="file"
-            filename="serviceImage"
-            onChange={(e) => onchange(e)}
-            //onChange={(e) => setFileName(e.target.value)}
-          />
-          <Button
-            style={{
-              backgroundColor: "#283b42",
-              color: "white",
-              padding: "4px 18px",
-            }}
-            type="submit"
-            onClick={submitHandler}
-          >
-            Add Service
-          </Button>
-        </form>
+        <Input
+          placeholder="Service Title"
+          type="text"
+          value={serviceName}
+          onChange={(e) => setServiceName(e.target.value)}
+        />
+        <Input
+          placeholder="Photo"
+          type="file"
+          name="serviceImage"
+          //value={fileName}
+          onChange={onchangeFile}
+        />
+        <Button
+          style={{
+            backgroundColor: "#283b42",
+            color: "white",
+            padding: "4px 18px",
+          }}
+          type="submit"
+          onClick={submitHandler}
+        >
+          Add Service
+        </Button>
       </div>
       <Button
         onClick={() => {
           localStorage.removeItem("userInfo");
+          props.setToken("");
           props.setToken("");
           history.push("/");
         }}
