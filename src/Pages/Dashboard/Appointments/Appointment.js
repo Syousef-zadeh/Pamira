2772c7b614e7b3309e73f895fa95ac99fec3ebc9
Sheida <syousef-zadeh@myseneca.ca>
2021-09-16 14:22:52 -0400

    Appoinment Page

--- src/Pages/Dashboard/Appointments/Appointment.js
+++ src/Pages/Dashboard/Appointments/Appointment.js
@@ -1,15 +1,14 @@
-import  React, {useState, useEffect} from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useState, useEffect } from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
@@ -20,31 +19,16 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default function Appointments() {
-
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const [booking, setBooking] = useState([]);
 
   useEffect(() => {
@@ -58,8 +42,6 @@ export default function Appointments() {
           });
       } catch (error) {
         console.log(error);
-        setIsLoading(true);
-        setError(null);
       }
     };
     fetchBooking();
@@ -79,7 +61,7 @@ export default function Appointments() {
         </TableHead>
         <TableBody>
           {booking.map((row) => (
-            <StyledTableRow >
+            <StyledTableRow>
               <StyledTableCell component="th" scope="row">
                 {row.fullName}
               </StyledTableCell>
